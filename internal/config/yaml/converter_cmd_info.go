a3225d61632787d549e1210a378616a854dd683f
seungyeop-lee <xbeast7@gmail.com>
2022-12-12 00:40:20 +0900

    스펙에 맞게 cmdInfo 생성 부분 수정

--- internal/config/yaml/converter_cmd_info.go
+++ internal/config/yaml/converter_cmd_info.go
@@ -19,61 +19,11 @@ func NewCmdInfoConverter(cmdInfo CmdInfo) *CmdInfoConverter {
 func (c CmdInfoConverter) Convert() domain.Cmd {
 	v := reflect.ValueOf(c.cmdInfo)
 	switch v.Kind() {
-	case reflect.String:
-		return c.mapToSingleCmd(v)
-	case reflect.Map:
-		return c.mapToStructuredCmd(v)
 	case reflect.Slice:
 		return c.mapForSlice(v)
 	default:
 		return domain.EmptyCmd{}
 	}
-}
-
-func (c CmdInfoConverter) mapToSingleCmd(v reflect.Value) domain.SingleCmd {
-	s := v.Interface().(string)
-	return domain.SingleCmd(s)
-}
-
-func (c CmdInfoConverter) mapToStructuredCmd(m reflect.Value) domain.StructuredCmd {
-	mapData := map[string]reflect.Value{}
-	for _, k := range m.MapKeys() {
-		v := m.MapIndex(k)
-		if v.Kind() == reflect.Interface {
-			v = v.Elem()
-		}
-		mapData[k.String()] = v
-	}
-
-	dir := mapData["dir"].Interface().(string)
-
-	switch mapData["cmd"].Kind() {
-	case reflect.String:
-		return domain.StructuredCmd{
-			Cmd: c.mapToSingleCmd(mapData["cmd"]),
-			Dir: domain.Path(dir),
-		}
-	case reflect.Slice:
-		return domain.StructuredCmd{
-			Cmd: c.mapForMultiLineCmd(mapData["cmd"]),
-			Dir: domain.Path(dir),
-		}
-	}
-
-	return domain.StructuredCmd{
-		Cmd: domain.EmptyCmd{},
-		Dir: "",
-	}
-}
-
-func (c CmdInfoConverter) mapForMultiLineCmd(v reflect.Value) domain.MultiLineCmd {
-	result := domain.MultiLineCmd{}
-	cmdInterfaces := v.Interface().([]interface{})
-	for _, cmdInterface := range cmdInterfaces {
-		v2 := reflect.ValueOf(cmdInterface)
-		result.Cmds = append(result.Cmds, c.mapToSingleCmd(v2))
-	}
-	return result
 }
 
 func (c CmdInfoConverter) mapForSlice(s reflect.Value) domain.Cmds {
@@ -86,9 +36,66 @@ func (c CmdInfoConverter) mapForSlice(s reflect.Value) domain.Cmds {
 			result = append(result, c.mapToSingleCmd(v))
 		case reflect.Map:
 			result = append(result, c.mapToStructuredCmd(v))
-		case reflect.Slice:
-			result = append(result, c.mapForMultiLineCmd(v))
 		}
+	}
+	return result
+}
+
+func (c CmdInfoConverter) mapToSingleCmd(v reflect.Value) domain.SingleCmd {
+	s := v.Interface().(string)
+	return domain.SingleCmd(s)
+}
+
+func (c CmdInfoConverter) mapToStructuredCmd(m reflect.Value) domain.Cmd {
+	mapData := map[string]reflect.Value{}
+	for _, k := range m.MapKeys() {
+		v := m.MapIndex(k)
+		if v.Kind() == reflect.Interface {
+			v = v.Elem()
+		}
+		mapData[k.String()] = v
+	}
+
+	switch mapData["cmd"].Kind() {
+	case reflect.String:
+		if isEmptyDir(mapData["dir"]) {
+			return c.mapToSingleCmd(mapData["cmd"])
+		}
+		return domain.StructuredCmd{
+			Cmd: c.mapToSingleCmd(mapData["cmd"]),
+			Dir: domain.Path(mapData["dir"].Interface().(string)),
+		}
+	case reflect.Slice:
+		if isEmptyDir(mapData["dir"]) {
+			return c.mapForMultiLineCmd(mapData["cmd"])
+		}
+		return domain.StructuredCmd{
+			Cmd: c.mapForMultiLineCmd(mapData["cmd"]),
+			Dir: domain.Path(mapData["dir"].Interface().(string)),
+		}
+	}
+
+	return domain.StructuredCmd{
+		Cmd: domain.EmptyCmd{},
+		Dir: "",
+	}
+}
+
+func isEmptyDir(dirValue reflect.Value) bool {
+	switch dirValue.Kind() {
+	case reflect.String:
+		return false
+	default:
+		return true
+	}
+}
+
+func (c CmdInfoConverter) mapForMultiLineCmd(v reflect.Value) domain.MultiLineCmd {
+	result := domain.MultiLineCmd{}
+	cmdInterfaces := v.Interface().([]interface{})
+	for _, cmdInterface := range cmdInterfaces {
+		v2 := reflect.ValueOf(cmdInterface)
+		result.Cmds = append(result.Cmds, c.mapToSingleCmd(v2))
 	}
 	return result
 }
